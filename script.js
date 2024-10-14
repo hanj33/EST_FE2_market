9e4059a706437072047e944e97ab34b731bd828c
han.j33 <jeehyun0212@naver.com>
2024-10-14 21:43:57 +0900

    메인페이지 html, css 수정

---
+++ script.js
@@ -0,0 +1,11 @@
+const imageSlideContainer = document.querySelector(".imageSlideContainer");
+const slideArrowLeft = imageSlideContainer.querySelector(".slideArrowLeft");
+const imageSlide = imageSlideContainer.querySelector(".imageSlide");
+const imageSlideBar = imageSlideContainer.querySelector(".imageSlideBar");
+const imageSlideBarBtn = imageSlideBar.querySelectorAll("button");
+
+slideArrowLeft.addEventListener("click", () => {
+  // 모르겟당ㅎㅎ...
+  // 만약 마지막 이미지가 아니라면 -> 옆으로 넘기기
+  // 만약 마지막 이미지라면 -> 처음으로 돌아가기..
+});
