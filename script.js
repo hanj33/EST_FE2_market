1ffe56a3752423160dbc56ef8e101e575100abf4
han.j33 <jeehyun0212@naver.com>
2024-10-15 17:04:21 +0900

    상세, 로그인, 404 html/css 수정

--- script.js
+++ script.js
@@ -3,9 +3,45 @@ const slideArrowLeft = imageSlideContainer.querySelector(".slideArrowLeft");
 const imageSlide = imageSlideContainer.querySelector(".imageSlide");
 const imageSlideBar = imageSlideContainer.querySelector(".imageSlideBar");
 const imageSlideBarBtn = imageSlideBar.querySelectorAll("button");
+const slideArrowRight = imageSlideContainer.querySelector(".slideArrowRight");
 
-slideArrowLeft.addEventListener("click", () => {
-  // 모르겟당ㅎㅎ...
-  // 만약 마지막 이미지가 아니라면 -> 옆으로 넘기기
-  // 만약 마지막 이미지라면 -> 처음으로 돌아가기..
-});
+// 이미지 개수
+const imageSlideLength =
+  imageSlide.querySelectorAll(".imageSlideImgCell").length - 1;
+let cellCount = 0;
+
+// function prevSlide() {
+//   if (cellCount !== 0) {
+//     cellCount--;
+//     imageSlideContainer.style.transform = `translateX(${cellCount * -192} rem`;
+
+//     console.log(cellCount);
+//   } else {
+//     cellCount = imageSlideLength;
+//     imageSlideContainer.style.transform = `translateX(${
+//       imageSlideLength * -192
+//     } rem`;
+
+//     console.log(cellCount);
+//   }
+// }
+// function nextSlide() {
+//   if (cellCount !== imageSlideLength) {
+//     imageSlideContainer.style.transform = `translateX(${
+//       (cellCount + 1) * -192
+//     } rem`;
+//     cellCount++;
+
+//     console.log(cellCount);
+//   } else {
+//     cellCount = 0;
+//     imageSlideContainer.style.transform = `translateX(0px)`;
+
+//     console.log(cellCount);
+//   }
+// }
+
+// slideArrowLeft.addEventListener("click", prevSlide);
+// slideArrowRight.addEventListener("click", nextSlide);
+
+slideArrowLeft.addEventListener("click", () => {});
