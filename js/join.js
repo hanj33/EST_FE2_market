932ec7e0ed5f1c51ff7bedd1799dad9d89c2af88
han.j33 <jeehyun0212@naver.com>
2024-10-17 10:45:42 +0900

    회원가입 html, css 수정

--- js/join.js
+++ js/join.js
@@ -3,6 +3,9 @@ const listOpenBtn = userPhoneNumContainer.querySelector(".listOpen");
 const phoneNumList = userPhoneNumContainer.querySelector(".phoneNumList");
 const img = userPhoneNumContainer.querySelector("img");
 
+const userHelp = document.querySelector(".userHelp");
+const agreeTermCheck = userHelp.querySelector(".agreeTermCheck");
+
 function selectPhoneNum(e) {
   if (e.target.nodeName === "BUTTON") {
     listOpenBtn.textContent = e.target.textContent;
@@ -22,3 +25,17 @@ listOpenBtn.addEventListener("click", () => {
 });
 
 phoneNumList.addEventListener("click", selectPhoneNum);
+
+agreeTermCheck.addEventListener("click", () => {
+  agreeTermCheck.classList.toggle("on");
+
+  if (agreeTermCheck.classList.contains("on")) {
+    img.setAttribute("src", "./assets/check-fill-box.svg");
+  } else {
+    img.setAttribute("src", "./assets/check-box.svg");
+  }
+});
+
+// 비밀번호 서로 일치하는지 확인
+
+// 전부 입력되고+체크박스 체크해야 가입하기 버튼 활성화...?
