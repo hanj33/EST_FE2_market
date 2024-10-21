5a5d03f1a58fe3fab8c6611e31ee20bcf15710e1
han.j33 <jeehyun0212@naver.com>
2024-10-21 13:42:07 +0900

    쓰지 않는 코드 삭제

--- js/item.js
+++ js/item.js
@@ -2,5 +2,3 @@ const itemContainer = document.getElementById("itemContainer");
 const item = document.createDocumentFragment();
 
 const url = "https://estapi.openmarket.weniv.co.kr/";
-
-fetch(url);
